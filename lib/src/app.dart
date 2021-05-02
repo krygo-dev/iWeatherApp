e6f41b35cab63e5c41c8679b156fa62e71d55ce4
Adam Chylaszek <adam.chylaszek98@gmail.com>
2021-05-02 19:17:27 +0200

    Signed in user check, home screen search bar

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:i_weather_app/src/screens/home.dart';
 import 'package:i_weather_app/src/screens/startup.dart';
 
 class App extends StatelessWidget {
-
+  final auth = FirebaseAuth.instance;
   final Map<int, Color> primaryColor =
   {
     50:Color.fromRGBO(42, 82, 97, .1),
@@ -25,7 +27,7 @@ class App extends StatelessWidget {
           accentColor: Color.fromARGB(255, 248, 208, 108),
           primarySwatch: MaterialColor(0xFF2A5261, primaryColor)
       ),
-      home: StartScreen(),
+      home: auth.currentUser != null ? HomeScreen() : StartScreen(),
     );
   }
 }
--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,19 +1,78 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:i_weather_app/src/screens/login.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final auth = FirebaseAuth.instance;
+
+  bool isSearching = false;
+  var _searchController = TextEditingController();
+  String search;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          child: Text('Logout'),
+      resizeToAvoidBottomInset: false,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: !isSearching
+            ? Text('')
+            : TextField(
+                autofocus: true,
+                controller: _searchController,
+                cursorColor: Theme.of(context).accentColor,
+                decoration: InputDecoration(
+                    hintText: 'Search here..',
+                    hintStyle: TextStyle(color: Colors.white),
+                    suffixIcon: IconButton(
+                      onPressed: () => _searchController.clear(),
+                      icon: Icon(Icons.clear,
+                          color: _searchController.text.isNotEmpty
+                              ? Theme.of(context).accentColor
+                              : Colors.transparent),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    )),
+                onChanged: (value) {
+                  setState(() {
+                    search = value.trim();
+                  });
+                },
+              ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.search, color: Colors.white, size: 30),
           onPressed: () {
-            Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) => LoginScreen()));
+            setState(() {
+              isSearching = !isSearching;
+            });
           },
         ),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.menu, color: Colors.white, size: 30),
+              onPressed: () {
+                auth.signOut().then((value) => Navigator.of(context)
+                    .pushReplacement(MaterialPageRoute(
+                        builder: (context) => LoginScreen())));
+              })
+        ],
+      ),
+      body: Container(
+        child: Stack(children: [
+          Container(
+            decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+          ),
+        ]),
       ),
     );
   }
